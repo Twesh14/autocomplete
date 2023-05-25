fc56cd47cfde1790de7d7e8b80952e58258c1170
Twesh14 <85089662+Twesh14@users.noreply.github.com>
2023-05-25 22:01:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+let names = [
+    "A1,TA1,TAA1,",
+    "A2,TA2,TAA2,",
+    "C1,TC1,TCC1,",
+  ];
+  //Sort names in ascending order
+  let sortedNames = names.sort();
+  
+  //reference
+  let input = document.getElementById("input");
+  let ss="";
+  let spl=[];
+  //Execute function on keyup
+  input.addEventListener("keyup", (e) => {
+    //loop through above array
+    //Initially remove all elements ( so if user erases a letter or adds new letter then clean previous outputs)
+    removeElements();
+    for (let i of sortedNames) {
+      //convert input to lowercase and compare with each string
+      // let kk = input.value.replace(ss, '');
+      spl = input.value.split(",");
+      if (
+        
+        i.toLowerCase().startsWith(spl[spl.length-1].toLowerCase()) &&
+        input.value != ""
+      ) {
+        //create li element
+        let listItem = document.createElement("li");
+        //One common class name
+        listItem.classList.add("list-items");
+        listItem.style.cursor = "pointer";
+        listItem.setAttribute("onclick", "displayNames('" + i + "')");
+        //Display matched part in bold
+        let word = "<b>" + i.substr(0, input.value.length) + "</b>";
+        word += i.substr(input.value.length);
+        //display the value in array
+        listItem.innerHTML = word;
+        document.querySelector(".list").appendChild(listItem);
+      }
+    }
+  });
+  function displayNames(value) {
+    if(spl.length==1){
+      input.value =value;
+    }else{
+      input.value =spl.slice(0,spl.length-1).join(",")+","+value;
+    }
+    input.focus();
+    // ss+=value;
+    removeElements();
+  }
+  function removeElements() {
+    //clear all the item
+    let items = document.querySelectorAll(".list-items");
+    items.forEach((item) => {
+      item.remove();
+    });
+  }
